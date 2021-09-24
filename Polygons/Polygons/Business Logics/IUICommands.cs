07daf638da25916869a9abc1fa716280ea03041a
Bef0r <alb.mark94@gmail.com>
2021-09-24 23:16:16 +0200

    Create ui commands interface with implement

---
+++ Polygons/Polygons/Business Logics/IUICommands.cs
@@ -0,0 +1,13 @@
+﻿using Polygons.Models.Polygons;
+using Polygons.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Business_Logics
+{
+    interface IUICommands
+    {
+        CanvasViewModel generateNewPolygon(NewPolygonParameters newPolygonParameters);
+    }
+}
---
+++ Polygons/Polygons/Business Logics/UICommandsImp.cs
@@ -0,0 +1,22 @@
+﻿using Polygons.Models.Polygons;
+using Polygons.Models.Shapes;
+using Polygons.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Polygons.Business_Logics
+{
+    class UICommandsImp : IUICommands
+    {
+        public CanvasViewModel generateNewPolygon(NewPolygonParameters newPolygonParameters)
+        {
+            Polygon polygons = new PolygonGenerator(newPolygonParameters.numberOfVerticesOfPolygon).setMaximumXAxisValue(newPolygonParameters.canvasHeight).setMaximumYAxisValue(newPolygonParameters.canvasHeight).build();
+            Canvas canvas = new Canvas();
+            canvas.Children.Add(polygons);
+            return new CanvasViewModel(canvas);
+        }
+    }
+}
