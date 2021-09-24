046dc1599fe4849da243159b1f45bd7f0c44a2df
Bef0r <alb.mark94@gmail.com>
2021-09-24 23:17:43 +0200

    Create data object. The class help pass the parameters from ui to business logic

---
+++ Polygons/Polygons/Models/Polygons/NewPolygonParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Models.Polygons
+{
+    class NewPolygonParameters
+    {
+        public int canvasWidth { get; }
+        public int canvasHeight { get; }
+        public int numberOfVerticesOfPolygon { get; }
+
+        public NewPolygonParameters(int canvasWidth, int canvasHeight, int numberOfVerticesOfPolygon)
+        {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
+            this.numberOfVerticesOfPolygon = numberOfVerticesOfPolygon;
+        }
+
+    }
+}
