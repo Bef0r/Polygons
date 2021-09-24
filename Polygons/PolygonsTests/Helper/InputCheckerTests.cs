31c2192aa9dbe61db43b524e05fb8e7fcf086402
Bef0r <alb.mark94@gmail.com>
2021-09-24 22:02:25 +0200

    auto gen

---
+++ Polygons/PolygonsTests/Helper/InputCheckerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polygons.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Helper.Tests
+{
+    [TestClass()]
+    public class InputCheckerTests
+    {
+        [TestMethod()]
+        public void NumberOfVerticesOfPolygonCheckerTest()
+        {
+            Assert.IsFalse( InputChecker.NumberOfVerticesOfPolygonChecker("2"));
+            Assert.IsFalse(InputChecker.NumberOfVerticesOfPolygonChecker("a"));
+            Assert.IsFalse(InputChecker.NumberOfVerticesOfPolygonChecker("?"));
+            Assert.IsFalse(InputChecker.NumberOfVerticesOfPolygonChecker("A"));
+            Assert.IsFalse(InputChecker.NumberOfVerticesOfPolygonChecker("1"));
+            Assert.IsTrue(InputChecker.NumberOfVerticesOfPolygonChecker("3"));
+            Assert.IsTrue(InputChecker.NumberOfVerticesOfPolygonChecker("10"));
+        }
+    }
+}
