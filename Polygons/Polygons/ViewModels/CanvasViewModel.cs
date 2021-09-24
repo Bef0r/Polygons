55b5624e9dee963eca2776a8e8601a7ef59e4fb3
Bef0r <alb.mark94@gmail.com>
2021-09-24 23:18:10 +0200

    create canvas view model.

---
+++ Polygons/Polygons/ViewModels/CanvasViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Polygons.ViewModels
+{
+    class CanvasViewModel
+    {
+        public CanvasViewModel(Canvas myCanvas)
+        {
+            this.myCanvas = myCanvas;
+        }
+
+        public Canvas myCanvas { get; set; }
+    }
+}
