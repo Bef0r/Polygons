65df4b3dcb7ee5bc0679c79954573131d7865eab
Bef0r <alb.mark94@gmail.com>
2021-09-25 22:36:35 +0200

    Create window settings model

---
+++ Polygons/Polygons/Models/Settings/MainWindowSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Models
+{
+    class MainWindowSettings
+    {
+        public MainWindowSettings(int height, int width, bool fullScreen)
+        {
+            this.height = height;
+            this.width = width;
+            this.fullScreen = fullScreen;
+        }
+
+        public int height { set; get; }
+        public int width { set; get; }
+        public Boolean fullScreen { set; get; }
+    }
+}
