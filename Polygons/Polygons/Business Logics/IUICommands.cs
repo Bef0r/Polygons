fa7e15f35e461607a091722d7866e092f794ae95
Bef0r <alb.mark94@gmail.com>
2021-09-25 22:35:52 +0200

    Create save settings method

--- Polygons/Polygons/Business Logics/IUICommands.cs
+++ Polygons/Polygons/Business Logics/IUICommands.cs
@@ -1,4 +1,5 @@
-﻿using Polygons.Models.Polygons;
+﻿using Polygons.Models;
+using Polygons.Models.Polygons;
 using Polygons.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -9,5 +10,6 @@ namespace Polygons.Business_Logics
     interface IUICommands
     {
         CanvasViewModel generateNewPolygon(NewPolygonParameters newPolygonParameters);
+        Boolean saveMainWindowSettings(MainWindowSettings mainWindowSettings);
     }
 }
