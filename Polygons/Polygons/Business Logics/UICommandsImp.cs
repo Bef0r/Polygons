d14950b13e7b169ae361d9a9550fd5cff2b70902
Bef0r <alb.mark94@gmail.com>
2021-09-25 22:36:13 +0200

    implement save settings method

--- Polygons/Polygons/Business Logics/UICommandsImp.cs
+++ Polygons/Polygons/Business Logics/UICommandsImp.cs
@@ -1,8 +1,11 @@
-﻿using Polygons.Models.Polygons;
+﻿using Nancy.Json;
+using Polygons.Models;
+using Polygons.Models.Polygons;
 using Polygons.Models.Shapes;
 using Polygons.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows.Shapes;
@@ -17,6 +20,39 @@ namespace Polygons.Business_Logics
             Canvas canvas = new Canvas();
             canvas.Children.Add(newPolygon);
             return new CanvasViewModel(canvas);
+        }
+
+        public bool saveMainWindowSettings(MainWindowSettings mainWindowSettings)
+        {
+            bool isFileWriteSuccessful = true;
+            StreamWriter outputFile = null;
+            try
+            {
+                writeSettingsToFile(ref mainWindowSettings,ref outputFile);
+            }
+            catch (Exception ex)
+            {
+                isFileWriteSuccessful = false;
+            }
+            finally
+            {
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+            return isFileWriteSuccessful;
+        }
+
+        protected void writeSettingsToFile(ref MainWindowSettings mainWindowSettings, ref StreamWriter outputFile)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            using (outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "settings.txt")))
+            {
+                var json = new JavaScriptSerializer().Serialize(mainWindowSettings);
+                outputFile.WriteLine(json);
+            }
+
         }
     }
 }
