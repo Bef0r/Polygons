56419db7cd931cf3ca603b2107284338625cc6b8
Bef0r <alb.mark94@gmail.com>
2021-09-26 12:43:23 +0200

    Implement load settings from file

--- Polygons/Polygons/Business Logics/SaveAndLoadFile/SaveAndLoadImp.cs
+++ Polygons/Polygons/Business Logics/SaveAndLoadFile/SaveAndLoadImp.cs
@@ -1,4 +1,6 @@
 ﻿using Nancy.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Polygons.Models;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,22 @@ namespace Polygons.Business_Logics.SaveAndLoadFile
 {
     class SaveAndLoadImp : ISaveAndLoad
     {
+        protected const String FILE_NAME = "settings.txt";
+        public MainWindowSettings loadMainWindowSettings()
+        {
+            try
+            {
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                docPath = docPath + "\\" + FILE_NAME;
+                return JsonConvert.DeserializeObject<MainWindowSettings>(File.ReadAllText(docPath));
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+
         public bool saveMainWindowSettings(MainWindowSettings mainWindowSettings)
         {
             bool isFileWriteSuccessful = true;
@@ -34,7 +52,7 @@ namespace Polygons.Business_Logics.SaveAndLoadFile
         protected void writeSettingsToFile(ref MainWindowSettings mainWindowSettings, ref StreamWriter outputFile)
         {
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            using (outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "settings.txt")))
+            using (outputFile = new StreamWriter(System.IO.Path.Combine(docPath, FILE_NAME)))
             {
                 var json = new JavaScriptSerializer().Serialize(mainWindowSettings);
                 outputFile.WriteLine(json);
