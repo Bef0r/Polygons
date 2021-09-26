bfc564e2319f566c66fd9b70c6e14d819e7bd9e4
Bef0r <alb.mark94@gmail.com>
2021-09-26 23:16:04 +0200

    Create calculate class

---
+++ Polygons/Polygons/Business Logics/CoordinateSystem/DistanceBetweenTwoPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Business_Logics.CoordinateSystem
+{
+    public class DistanceBetweenTwoPoint
+    {
+        public static double calculateDistanceBetweenTwoPoint(int firstPointXCoordiante, int firstPointYCoordiante, int secondPointXCoordiante, int secondPointYCoordiante)
+        {
+            return Math.Sqrt(subtractionAndPow(firstPointXCoordiante, secondPointXCoordiante) + subtractionAndPow(firstPointYCoordiante, secondPointYCoordiante));
+        }
+
+        protected static double subtractionAndPow(int firstCoordiante, int secondCoordiante)
+        {
+            return Math.Pow((Convert.ToDouble(secondCoordiante) - Convert.ToDouble(firstCoordiante)), 2);
+        }
+
+    }
+}
