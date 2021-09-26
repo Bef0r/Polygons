c1ff7d07b3b1f9e1fb554a4ea456230aed996876
Bef0r <alb.mark94@gmail.com>
2021-09-26 12:10:27 +0200

    Implement save method

---
+++ Polygons/Polygons/Business Logics/SaveAndLoadFile/SaveAndLoadImp.cs
@@ -0,0 +1,45 @@
+﻿using Nancy.Json;
+using Polygons.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Polygons.Business_Logics.SaveAndLoadFile
+{
+    class SaveAndLoadImp : ISaveAndLoad
+    {
+        public bool saveMainWindowSettings(MainWindowSettings mainWindowSettings)
+        {
+            bool isFileWriteSuccessful = true;
+            StreamWriter outputFile = null;
+            try
+            {
+                writeSettingsToFile(ref mainWindowSettings, ref outputFile);
+            }
+            catch (Exception ex)
+            {
+                isFileWriteSuccessful = false;
+            }
+            finally
+            {
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+            return isFileWriteSuccessful;
+        }
+
+        protected void writeSettingsToFile(ref MainWindowSettings mainWindowSettings, ref StreamWriter outputFile)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            using (outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "settings.txt")))
+            {
+                var json = new JavaScriptSerializer().Serialize(mainWindowSettings);
+                outputFile.WriteLine(json);
+            }
+
+        }
+    }
+}
