3b280738ba93b80c24a1e93934637e7a59bd2cc1
Bef0r <alb.mark94@gmail.com>
2021-09-26 23:15:31 +0200

    Add storage to class. From now on the class storage the genarate points of the polygons

--- Polygons/Polygons/Business Logics/UICommandsImp.cs
+++ Polygons/Polygons/Business Logics/UICommandsImp.cs
@@ -16,14 +16,17 @@ namespace Polygons.Business_Logics
     class UICommandsImp : IUICommands, ISaveAndLoad
     {
         private ISaveAndLoad saveAndLoad;
+        private GeneratePolygonsStorage storage;
         public UICommandsImp()
         {
             saveAndLoad = new SaveAndLoadImp();
+            storage = GeneratePolygonsStorage.getInstance();
         }
 
         public CanvasViewModel generateNewPolygon(NewPolygonParameters newPolygonParameters)
         {
             Polygon newPolygon = new PolygonGenerator(newPolygonParameters.numberOfVerticesOfPolygon).setMaximumXAxisValue(newPolygonParameters.canvasWidth).setMaximumYAxisValue(newPolygonParameters.canvasHeight).build();
+            storage.addNewPoints(newPolygon.Points);
             Canvas canvas = new Canvas();
             canvas.Children.Add(newPolygon);
             return new CanvasViewModel(canvas);
