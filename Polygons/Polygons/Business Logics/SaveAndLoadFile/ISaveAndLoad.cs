1fcc791b8806683ee5a75bd8813872041c9c568a
Bef0r <alb.mark94@gmail.com>
2021-09-26 12:10:15 +0200

    Create save and load interface

---
+++ Polygons/Polygons/Business Logics/SaveAndLoadFile/ISaveAndLoad.cs
@@ -0,0 +1,9 @@
+﻿using Polygons.Models;
+
+namespace Polygons.Business_Logics.SaveAndLoadFile
+{
+    interface ISaveAndLoad
+    {
+        bool saveMainWindowSettings(MainWindowSettings mainWindowSettings1);
+    }
+}
