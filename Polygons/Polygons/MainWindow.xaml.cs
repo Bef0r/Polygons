d62d605f13a05e46c281d948f1b0045ac5bbc31f
Bef0r <alb.mark94@gmail.com>
2021-09-26 12:10:54 +0200

    Remove unuse imports

--- Polygons/Polygons/MainWindow.xaml.cs
+++ Polygons/Polygons/MainWindow.xaml.cs
@@ -1,25 +1,10 @@
-﻿using Nancy.Json;
-using Polygons.Business_Logics;
+﻿using Polygons.Business_Logics;
 using Polygons.Helper;
 using Polygons.Models;
 using Polygons.Models.Polygons;
-using Polygons.Models.Shapes;
 using Polygons.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Polygons
 {
