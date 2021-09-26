de21a0f8c72f8312cc554e4633e6b04f2363a032
Bef0r <alb.mark94@gmail.com>
2021-09-26 23:17:09 +0200

    Create calculate class test

---
+++ Polygons/PolygonsTests/Business Logics/CoordinateSystem/DistanceBetweenTwoPointTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polygons.Business_Logics.CoordinateSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygons.Business_Logics.CoordinateSystem.Tests
+{
+    [TestClass()]
+    public class DistanceBetweenTwoPointTests
+    {
+        [TestMethod()]
+        public void calculateDistanceBetweenTwoPointTest()
+        {
+            Assert.AreEqual(DistanceBetweenTwoPoint.calculateDistanceBetweenTwoPoint(0, 0, 0, 1), 1);
+            Assert.AreEqual(DistanceBetweenTwoPoint.calculateDistanceBetweenTwoPoint(5, 0, 0, 0), 5);
+            Assert.AreEqual(DistanceBetweenTwoPoint.calculateDistanceBetweenTwoPoint(10, 4, 3, 0), 8,0622577482985);
+            
+        }
+    }
+}
